a8b9cceb3ccc5f8e22106746bfbfb2822532467d
Hekku2 <hekku2@gmail.com>
2017-02-07 17:54:09 +0200

    Fixed test warning

--- lan-steam-frontend/src/components/Game/Game.test.js
+++ lan-steam-frontend/src/components/Game/Game.test.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import Game from './Game';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
+    const tbody = document.createElement('tbody');
 
     var game = {
         owners: []
     };
 
-    ReactDOM.render(<Game game={game}/>, div);
+    ReactDOM.render(<Game game={game}/>, tbody);
 });
